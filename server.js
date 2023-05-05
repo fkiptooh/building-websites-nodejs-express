bc8655cc0a3fe3b485db33a860e87c9c532cd992
fkiptooh <fkiptooh@gmail.com>
2023-05-05 04:22:01 +0300

    Created a routing module

--- server.js
+++ server.js
@@ -3,21 +3,16 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const routes = require('./routes');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
+app.use('/', routes());
+
 const port = 3000;
-
-app.get('/', (req, res) => {
-  res.render('pages/index', { pageTitle: 'Welcome' });
-});
-
-app.get(`/speakers`, (req, res) => {
-  res.sendFile(path.join(__dirname, './static/speakers.html'));
-});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
