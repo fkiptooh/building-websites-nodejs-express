6fc45f4db68f1e8e24faf339a312bfaf5dda48fe
fkiptooh <fkiptooh@gmail.com>
2023-05-06 01:38:43 +0300

    Dynamically rendering speakes

--- routes/speakers.js
+++ routes/speakers.js
@@ -6,7 +6,7 @@ module.exports = (params) => {
   const { speakersService } = params;
   router.get('/', async (req, res) => {
     const speakers = await speakersService.getList();
-    return res.json(speakers);
+    res.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers });
   });
 
   router.get('/:shortname', (req, res) => {
