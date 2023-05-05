05bf9e9c1f45b78d89907aa4effeff1e3555b360
fkiptooh <fkiptooh@gmail.com>
2023-05-05 03:53:41 +0300

    Installed ejs a javascript templating engine and configured it for templating

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 app.use(express.static(path.join(__dirname, './static')));
 
 const port = 3000;
