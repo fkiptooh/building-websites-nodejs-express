ca7f654bbdc443083fa708f2f30efd57d5435310
fkiptooh <fkiptooh@gmail.com>
2023-05-05 15:15:47 +0300

    Creating a site-wide layout

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,12 @@ const feedbackRoute = require('./feedback');
 
 module.exports = (params) => {
   router.get('/', (req, res) => {
-    if (!req.session.visitcount) {
-      req.session.visitcount = 0;
-    }
-    req.session.visitcount += 1;
-    console.log(`Number of visits: ${req.session.visitcount}`);
-    res.render('pages/index', { pageTitle: 'Welcome' });
+    // if (!req.session.visitcount) {
+    //   req.session.visitcount = 0;
+    // }
+    // req.session.visitcount += 1;
+    // console.log(`Number of visits: ${req.session.visitcount}`);
+    res.render('layout', { pageTitle: 'Welcome', template: 'index' });
   });
 
   router.use(`/speakers`, speakersRoute(params));
