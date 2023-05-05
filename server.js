de688f4617a29bf9d92abf2ac5433db804ae8422
fkiptooh <fkiptooh@gmail.com>
2023-05-05 03:38:33 +0300

    Added and a configured prettier alongside eslint for code formating

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
 
-const path = require('path')
+const path = require('path');
 
 const app = express();
-app.use(express.static(path.join(__dirname, './static')))
+app.use(express.static(path.join(__dirname, './static')));
 
 const port = 3000;
 
-app.get(`/`, (req, res)=> {
-    res.sendFile(path.join(__dirname, './static/index.html'));
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/index.html'));
+});
 
-app.get(`/speakers`, (req, res)=> {
-    res.sendFile(path.join(__dirname, './static/speakers.html'));
-})
+app.get(`/speakers`, (req, res) => {
+  res.sendFile(path.join(__dirname, './static/speakers.html'));
+});
 
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
