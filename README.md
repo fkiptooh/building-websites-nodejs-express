3ea68cac9cbc29696d768d24a74f36568505999f
fkiptooh <fkiptooh@gmail.com>
2023-05-05 03:05:59 +0300

    Readme

---
+++ README.md
@@ -0,0 +1,2 @@
+## Building Websites With NodeJs and Express
+This is a LinkedIn guided course whose resources can be found on ![Resources](https://github.com/danielkhan/building-website-nodejs-express.git)
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 
+const path = require('path')
+
 const app = express();
 
 const port = 3000;
 
-app.get(`/`, (req, res)=>{
-    res.send('Hello from express');
-});
+app.get(`/`, (req, res)=> {
+    res.sendFile(path.join(__dirname, './static/index.html'));
+})
+
+app.get(`/speakers`, (req, res)=> {
+    res.sendFile(path.join(__dirname, './static/speakers.html'));
+})
 
 app.listen(port, ()=>{
-    console.log(`Express starting on port ${port}`)
+    console.log(`Server running on port ${port}`)
 })
