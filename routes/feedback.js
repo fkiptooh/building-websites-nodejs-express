4457f82b197d2df79ad95d5aa66b3faace586704
fkiptooh <fkiptooh@gmail.com>
2023-05-07 14:56:08 +0300

    Storing data

--- routes/feedback.js
+++ routes/feedback.js
@@ -11,12 +11,14 @@ module.exports = (params) => {
       const feedback = await feedbackService.getList();
 
       const errors = req.session.feedback ? req.session.feedback.errors : false;
+      const successMessage = req.session.feedback ? req.session.feedback.message : false;
       req.session.feedback = {};
       return res.render('layout', {
         pageTitle: 'Feedback',
         template: 'feedback',
         feedback,
         errors,
+        successMessage,
       });
     } catch (error) {
       return next(error);
@@ -34,7 +36,7 @@ module.exports = (params) => {
       check('title').trim().isLength({ min: 3 }).escape().withMessage('Title is required'),
       check('message').trim().isLength({ min: 5 }).escape().withMessage('Message is required'),
     ],
-    (req, res) => {
+    async (req, res) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         req.session.feedback = {
@@ -42,7 +44,13 @@ module.exports = (params) => {
         };
         return res.redirect('/feedback');
       }
-      return res.send('Feedback posted');
+      const { name, email, title, message } = req.body;
+
+      await feedbackService.addEntry(name, email, title, message);
+      req.session.feedback = {
+        message: 'Thank you for your feedback',
+      };
+      return res.redirect('/feedback');
     }
   );
 
